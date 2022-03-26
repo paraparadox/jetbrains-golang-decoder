ddcbbdfe9c887561cec6b6b4a340ccee51a6d896
Siyovush <paraparadox@protonmail.com>
2022-03-26 21:01:50 +0500

    Stage 2/3: WIP

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	var s string
-	fmt.Scan(&s)
-	fmt.Println("OK")
+	reader := bufio.NewReader(os.Stdin)
+	s, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input := strings.Split(strings.Split(s, "\n")[0], " ")
+
+	g, err := strconv.ParseInt(input[2], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p, err := strconv.ParseInt(input[6], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b := rand.Int63n(p)
+
+	B := int64(math.Pow(float64(g), float64(b))) % p
+	fmt.Println(B)
 }
