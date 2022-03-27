af53b6e1299b882120e05ec3410fa28a60c8f4b4
Siyovush <paraparadox@protonmail.com>
2022-03-27 21:36:37 +0500

    Stage 3/3: WIP

--- main.go
+++ main.go
@@ -54,5 +54,38 @@ func main() {
 	for i = 0; i < b; i++ {
 		S = (S * A) % p
 	}
+	S %= 26
 	fmt.Println("B is", B)
+
+	// [65; 90] A-Z
+	// [97; 122] a-z
+
+	message := "Will you marry me?"
+	encryptedMessage := make([]byte, len(message))
+	for i = 0; i < int64(len(message)); i++ {
+		if int(message[i]) > 64 && int(message[i]) < 91 {
+			encryptedMessage[i] = byte((int64(message[i]) + S) % 91 + 65)
+		} else if int(message[i]) > 96 && int(message[i]) < 123 {
+			encryptedMessage[i] = byte((int64(message[i]) + S) % 123 + 97)
+		} else {
+			encryptedMessage[i] = message[i]
+		}
+	}
+	fmt.Println(encryptedMessage, string(encryptedMessage[:]))
+	message, err = reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	message = strings.Split(message, "\n")[0]
+	decryptedMessage := make([]byte, len(message))
+	for i = 0; i < int64(len(message)); i++ {
+		if int(message[i]) > 64 && int(message[i]) < 91 {
+			encryptedMessage[i] = byte(int64(message[i]) - 65 + 91)
+		} else if int(message[i]) > 96 && int(message[i]) < 123  {
+			encryptedMessage[i] = byte(int64(message[i]) - 97 + 123)
+		} else {
+			encryptedMessage[i] = message[i]
+		}
+	}
+	fmt.Println(string(decryptedMessage[:]))
 }
